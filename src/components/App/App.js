be9abd95c103c36580e3a4448cc60dfedeec37c1
Kerry Burns <kerryalanburns@gmail.com>
2018-04-20 20:04:44 -0500

    router started

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import Nav from '../Nav/Nav';
+
 
 class App extends Component {
   render() {
@@ -10,6 +12,7 @@ class App extends Component {
           <h1 className="App-title">Oh, the places you've been</h1>
           <h4><i>Reflection Board</i></h4>
         </header>
+          <Nav />
         <br/>
       </div>
     );
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+    redner(){
+        return(
+            <Router>
+            </Router>
+        )
+    }
+}
+
+
+export default Nav;

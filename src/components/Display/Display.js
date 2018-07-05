1e7ab0958159c1bd4492e221cf46aad2408e9b6b
Kerry Burns <kerryalanburns@gmail.com>
2018-07-05 16:19:42 -0500

    display changes

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './Display.css';
-import { Panel } from 'react-bootstrap';
+import { Panel, Grid, Row, Col } from 'react-bootstrap';
 import moment from 'moment';
 
 const mapStateToProps = reduxState => ({
@@ -40,21 +40,31 @@ class Display extends Component{
     render(){
         let date = moment(this.props.reduxState.reflectionReducer.date).format('MMMM Do YYYY');
 
-        let reflectionsArray = this.props.reduxState.reflectionReducer.map((reflection) => {
-            return (<Panel key={reflection.id}><h3>{reflection.topic}</h3> <div>{reflection.description} </div>
-                            {reflection.bookmarked} {date} 
-                            <div><button onClick={()=>this.deleteReflection(reflection)}>Delete</button></div>
-                            <div><button onClick={()=>this.bookmarkReflection(reflection)}>Bookmark</button></div>
-                            </Panel>)
-                    });
+    let reflectionsArray = this.props.reduxState.reflectionReducer.map((reflection) => {
+        return (<div key={reflection.id}>
+                    <Panel className="displayItem">
+                        <h3>{reflection.topic}</h3>
+                        <p>{reflection.description}</p>
+                        <p>{reflection.bookmarked}</p><p>{date}</p>          
+                        <button onClick={()=>this.deleteReflection(reflection)}>Delete</button>
+                        <button onClick={()=>this.bookmarkReflection(reflection)}>Bookmark</button>
+                    </Panel></div>)
+                });
 
-            return(
-                <div>
-                    
-                    <h2>View Reflections</h2>
-                    <div className="displayItem"> {reflectionsArray}</div>
-                </div>
-            )
+        return(
+            <div>
+                <Grid>
+                    <Row>
+                        <Col xs={12} md={12}>
+                        <h2><strong>View Reflections</strong></h2>
+                        </Col>
+                        <Col xs={12} sm={6} md={4} lg={4}>
+                        <div className="displayWrapper"> {reflectionsArray}</div>
+                     </Col>
+                    </Row>
+                </Grid>
+            </div>
+        )
     }
 }
 
